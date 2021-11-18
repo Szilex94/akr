07a80b47827282b4ff1bd267827e5ed7b167ead9
szilard.mate <szilard.mate@treedev.site>
2021-11-18 22:41:24 +0200

    Lowevered pi4j version

--- src/main/java/com/github/szilex94/service/SimpleService.java
+++ src/main/java/com/github/szilex94/service/SimpleService.java
@@ -1,11 +1,11 @@
 package com.github.szilex94.service;
 
-import com.pi4j.Pi4J;
-import com.pi4j.context.Context;
-import com.pi4j.io.gpio.digital.DigitalOutput;
-import com.pi4j.io.gpio.digital.DigitalOutputConfigBuilder;
-import com.pi4j.io.gpio.digital.DigitalState;
-import com.pi4j.platform.Platforms;
+import com.pi4j.io.gpio.GpioController;
+import com.pi4j.io.gpio.GpioFactory;
+import com.pi4j.io.gpio.GpioPinDigitalOutput;
+import com.pi4j.io.gpio.PinState;
+import com.pi4j.io.gpio.RaspiPin;
+
 import jakarta.annotation.PostConstruct;
 import jakarta.annotation.PreDestroy;
 import jakarta.inject.Singleton;
@@ -13,39 +13,24 @@ import jakarta.inject.Singleton;
 @Singleton
 public class SimpleService {
 
-	private static final int PIN_LED = 22; // PIN 15 = BCM 22
+	private final GpioController gpio = GpioFactory.getInstance();
 
-	private Context pi4j;
-
-	private DigitalOutput led;
+	private GpioPinDigitalOutput myLed;
 
 	@PostConstruct
 	public void postConstruct() {
-		pi4j = Pi4J.newAutoContext();
-		Platforms platforms = pi4j.platforms();
-
-		DigitalOutputConfigBuilder ledConfig = DigitalOutput.newConfigBuilder(pi4j)//
-				.id("led")//
-				.name("LED Flasher")//
-				.address(PIN_LED)//
-				.shutdown(DigitalState.LOW)//
-				.initial(DigitalState.LOW)//
-				.provider("pigpio-digital-output");
-
-		led = pi4j.create(ledConfig);
-
+		myLed = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_22, "My LED", PinState.LOW);
 	}
 
 	public void toogle(boolean state) {
 		System.out.println("Desired State " + state);
-		led.setState(state);
+		myLed.setState(state);
 	}
 
 	@PreDestroy
 	public void preDestroy() {
 		System.err.print("PreDestroy!");
-		System.out.println("Desired State OFF!");
-		pi4j.shutdown();
+		gpio.shutdown();
 	}
 
 }
