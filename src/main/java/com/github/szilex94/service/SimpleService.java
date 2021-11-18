b81dd2a71a95441ed1936421699c9cce1fcffd3d
szilard.mate <szilard.mate@treedev.site>
2021-11-18 23:05:40 +0200

    Added shutdown for GPIO pin

--- src/main/java/com/github/szilex94/service/SimpleService.java
+++ src/main/java/com/github/szilex94/service/SimpleService.java
@@ -3,6 +3,7 @@ package com.github.szilex94.service;
 import com.pi4j.io.gpio.GpioController;
 import com.pi4j.io.gpio.GpioFactory;
 import com.pi4j.io.gpio.GpioPinDigitalOutput;
+import com.pi4j.io.gpio.PinPullResistance;
 import com.pi4j.io.gpio.PinState;
 import com.pi4j.io.gpio.RaspiPin;
 
@@ -30,6 +31,7 @@ public class SimpleService {
 	@PreDestroy
 	public void preDestroy() {
 		System.err.print("PreDestroy!");
+		myLed.setShutdownOptions(true, PinState.LOW, PinPullResistance.OFF);
 		gpio.shutdown();
 	}
 
